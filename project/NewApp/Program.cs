fe5032bd6603494cfc3715879aa2e912cc2b69f1
ltmaianh <maianh2k2tx@gmail.com>
2023-09-10 22:03:56 +0700

    htmlBienSo

--- project/NewApp/Program.cs
+++ project/NewApp/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+internal class Program 
+{
+private static void Main(string[] args)
+{
+
+
+/*int a = 1;
+long b = 454;
+float c = 2.3f;
+double d =3.4;
+decimal e = 3;
+bool t = true;
+char kt = 'b';*/
+string Ten = "Le Thi Mai Anh";
+Console.WriteLine("Ho ten {0}",Ten);
+}
+}
