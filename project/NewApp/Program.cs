336373bbe67150779e1bcb0ffa6e093af0258b81
ltmaianh <maianh2k2tx@gmail.com>
2023-09-28 00:17:16 +0700

    objectFruit

--- project/NewApp/Program.cs
+++ project/NewApp/Program.cs
@@ -4,13 +4,11 @@ using NewApp.Models;
 public class Program
 {
     public static void Main(string[]args){
-        Employee ps1 = new Employee();
-        Employee ps2 = new Employee();
+        Fruit ps1 = new Fruit();
+        Fruit ps2 = new Fruit();
 
-        ps1.TenNhanVien = "Nguyen van a";
-        ps1.MaNhanVien = "a123456";
-        ps1.Tuoi = 21;
-        ps1.Luong =5000000;
+        ps1.Ten = "Cam";
+        ps1.Vi = "Chua";
         ps1.HienThiThongTin();
 
         ps2.NhapThongTin();
