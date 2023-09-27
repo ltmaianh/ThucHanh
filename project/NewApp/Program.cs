eac33c005f991f585afaaccdf3186961d223a352
ltmaianh <maianh2k2tx@gmail.com>
2023-09-28 00:09:30 +0700

    objectStudent

--- project/NewApp/Program.cs
+++ project/NewApp/Program.cs
@@ -1,5 +1,20 @@
-﻿using System.Collections.Specialized;
+﻿using System.Security.Cryptography;
+using System.Collections.Specialized;
 using NewApp.Models;
-Student std= new Student();
-std.NhapThongTin();
-std.HienThiThongTin();
+public class Program
+{
+    public static void Main(string[]args){
+        Student ps1 = new Student();
+        Student ps2 = new Student();
+
+        ps1.TenSinhVien = "Le Thi Mai Anh";
+        ps1.MaSinhVien = "2021050076";
+        ps1.Tuoi = 21;
+        ps1.KhoaHoc ="K65";
+        ps1.HienThiThongTin();
+
+        ps2.NhapThongTin();
+        ps2.HienThiThongTin();
+
+    }
+}
