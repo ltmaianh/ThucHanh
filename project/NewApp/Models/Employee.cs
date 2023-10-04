474dfdc145b4b7efdb165f628a81e42270adfdf7
ltmaianh <maianh2k2tx@gmail.com>
2023-10-04 15:45:23 +0700

    PTcogiatritraveEmployee

--- project/NewApp/Models/Employee.cs
+++ project/NewApp/Models/Employee.cs
@@ -5,8 +5,10 @@ namespace NewApp.Models{
         public int Tuoi { get; set; }
         public int Luong { get; set; }
        
-        public void ThongTin(string MaNhanVien,string TenNhanVien,int Tuoi,int Luong){
-             System.Console.WriteLine("{0} - {1} - {2} - {3} VND", MaNhanVien, TenNhanVien, Tuoi, Luong);
+        public int TinhLuong(int Luong){
+            int TLuong = Luong + 500000;
+            return TLuong;
+
         }
     }
 }
--- project/NewApp/Program.cs
+++ project/NewApp/Program.cs
@@ -9,8 +9,7 @@ public class Program
       string MaNhanVien = "2021050076"; 
        string TenNhanVien = "LeThiMaiAnh";
        int Tuoi = 21;
-       int Luong =3000000;
-       emp.ThongTin(MaNhanVien,TenNhanVien,Tuoi,Luong);
-       
+       int a = 3000000;
+     System.Console.WriteLine("{0}-{1}-{2}-{3}",MaNhanVien,TenNhanVien,Tuoi,emp.TinhLuong(a));
     }
 }
