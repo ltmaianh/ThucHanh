df671ed49fe3f0cfb8cca8ee28bac6c282fbc60b
ltmaianh <maianh2k2tx@gmail.com>
2023-10-04 15:56:41 +0700

    PTkhoitaoFruit

--- project/NewApp/Models/Fruit.cs
+++ project/NewApp/Models/Fruit.cs
@@ -1,14 +1,13 @@
+//Le Thi Mai Anh-2021050076
 namespace NewApp.Models{
-    public class Fruit{
+   public class Fruit{
         public string Ten { get; set; }
         public string Vi { get; set; }
-        public void NhapThongTin (){
-            System.Console.WriteLine("Ten hoa qua :");
-            Ten=Console.ReadLine();
-            System.Console.WriteLine("Vi Hoa qua:");
-            Vi=Console.ReadLine();
+        public Fruit(){
+            Ten = "Tao";
+            Vi = "Ngot";
+        }
         
-}
 public void HienThiThongTin(){
     System.Console.WriteLine("{0}-{1}",Ten,Vi);
 }
--- project/NewApp/Program.cs
+++ project/NewApp/Program.cs
@@ -4,12 +4,7 @@ public class Program
     public static void Main(string[]args)
     {
 
-        Employee emp = new Employee();
-     
-      string MaNhanVien = "2021050076"; 
-       string TenNhanVien = "LeThiMaiAnh";
-       int Tuoi = 21;
-       int a = 3000000;
-     System.Console.WriteLine("{0}-{1}-{2}-{3}",MaNhanVien,TenNhanVien,Tuoi,emp.TinhLuong(a));
+        Fruit frt= new Fruit();
+        frt.HienThiThongTin();
     }
 }
