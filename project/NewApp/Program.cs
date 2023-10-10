9fdd2d4d60e4b88cfc12f64c76e78eb27383cf73
ltmaianh <maianh2k2tx@gmail.com>
2023-10-11 01:29:48 +0700

    ArrayEmployee

--- project/NewApp/Program.cs
+++ project/NewApp/Program.cs
@@ -3,9 +3,40 @@ public class Program
 {
     public static void Main(string[]args)
     {
+      int n;
 
-     Fruit frt=new Fruit();
-      frt.NhapThongTin();
-      frt.HienThiThongTin();
+      Console.Write("\n" + "Nhap kich thuoc mang Employee: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        Employee[] empArr = new Employee[n];
+        for(int i = 0; i < empArr.Length; i++){
+            Console.WriteLine("Nhap thong tin nhan vien thu {0}", i+1);
+            Employee emp = new Employee();
+            emp.NhapThongTin();
+            empArr[i] = emp;
+        }
+        Console.WriteLine("Hien thi mang Employee");
+        foreach(Employee emp in empArr){
+            emp.HienThiThongTin();
+        }
+        string TenNhanVien = null;
+        System.Console.WriteLine($"Nhap ten nhan vien can thay doi: {TenNhanVien}");
+        TenNhanVien = Console.ReadLine();
+          for(int i = 0; i < empArr.Length; i++)
+        {   
+            Employee emp = (Employee) empArr[i];
+             if ( emp.TenNhanVien == TenNhanVien){
+
+                emp.NhapThongTin();
+                break;
+                 }else
+            {
+                System.Console.WriteLine("Khong co thong tin!");
+                break;
+            }
     }
+    Console.WriteLine("Hien thi mang Employee");
+        foreach(Employee emp in empArr){
+            emp.HienThiThongTin();
+}
+}
 }
