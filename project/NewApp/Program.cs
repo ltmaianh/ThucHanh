9ac02e186eff84302964687d877a17b12119845b
ltmaianh <maianh2k2tx@gmail.com>
2023-10-11 12:41:59 +0700

    StuArrayList-Student

--- project/NewApp/Program.cs
+++ project/NewApp/Program.cs
@@ -1,43 +1,58 @@
-﻿﻿using NewApp.Models;
+﻿﻿using System.Collections.ObjectModel;
+using NewApp.Models;
+﻿using System.Collections;
 //Le Thi Mai Anh-2021050076
 public class Program
 {
     public static void Main(string[]args)
     {
+        ArrayList StudentList = new ArrayList();
+
       int n;
-
-      Console.Write("\n" + "Nhap kich thuoc mang Customer: ");
+       System.Console.WriteLine("So phan tu muon them vao:");
         n = Convert.ToInt32(Console.ReadLine());
-        Customer[] cusArr = new Customer[n];
-        for(int i = 0; i < cusArr.Length; i++){
-            Console.WriteLine("Nhap thong tin khach hang thu {0}", i+1);
-            Customer cus = new Customer();
-            cus.NhapThongTin();
-            cusArr[i] = cus;
+      for(int i = 0; i < n; i++)
+{
+    Student std = new Student ();
+    std. NhapThongTin();
+    StudentList.Add(std);
+}
+ foreach(Student std in StudentList){
+            std.HienThiThongTin();
         }
-        Console.WriteLine("Hien thi mang Customer");
-        foreach(Customer cus in cusArr){
-            cus.HienThiThongTin();
-        }
-        string TenKhachHang = null;
-        System.Console.WriteLine($"Nhap ten khach hang can thay doi: {TenKhachHang}");
-        TenKhachHang = Console.ReadLine();
-          for(int i = 0; i < cusArr.Length; i++)
-        {   
-            Customer cus = (Customer) cusArr[i];
-             if (cus.TenKhachHang == TenKhachHang){
-
-                cus.NhapThongTin();
-                break;
+        string TenSinhVien = null;
+        System.Console.WriteLine($"Nhap ten sinh vien can thay doi: {TenSinhVien}");
+        TenSinhVien = Console.ReadLine();
+          for(int i = 0; i < StudentList.Count; i++)
+           {   
+             Student std = (Student) StudentList[i];
+             if (std.TenSinhVien == TenSinhVien){
+                 std.NhapThongTin();
+                  break;
                  }else
             {
                 System.Console.WriteLine("Khong co thong tin!");
                 break;
             }
     }
-    Console.WriteLine("Hien thi mang Customer");
-        foreach(Customer cus in cusArr){
-            cus.HienThiThongTin();
-}
+    foreach(Student std in StudentList){
+            std.HienThiThongTin();
+        }
+         System.Console.WriteLine("Nhap ten hoc sinh can xoa: ");
+        TenSinhVien = Console.ReadLine();
+
+        for(int i=0; i < StudentList.Count; i++ )
+        {
+            Student std = (Student) StudentList[i];
+            if(std.TenSinhVien == TenSinhVien)
+            {
+                StudentList.RemoveAt(i);
+                System.Console.WriteLine($"da xoa hoc sinh co ten la: {TenSinhVien}");
+                break;
+            }
+        } 
+        foreach(Student std in StudentList){
+            std.HienThiThongTin();
+        }
 }
 }
