099ecdf16859728624866636457c0e70255705c2
ltmaianh <maianh2k2tx@gmail.com>
2023-11-01 23:40:28 +0700

    Employee

--- DemoMVC/Controllers/EmployeeController.cs
+++ DemoMVC/Controllers/EmployeeController.cs
@@ -1,14 +1,15 @@
+using DemoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-
+//Le Thi Mai Anh-2021050076
 namespace DemoMVC.Controllers{
- public class Employee:Controller{
+ public class EmployeeController:Controller{
       public IActionResult Index(){
          return View();
         }
  [HttpPost]
-public IActionResult Index(string FName,string EmployeeID)
+public IActionResult Index(Employee emp)
  {
-      string strResult = "Hello " + FName +" "+EmployeeID;
+      string strResult = "Hello " + emp.FullName+" "+emp.EmployeeId;
        ViewBag.ThongBao = strResult;
       return View();
  }
---
+++ DemoMVC/Models/Employee.cs
@@ -0,0 +1,7 @@
+namespace DemoMVC.Models{
+    //Le Thi Mai Anh-2021050076
+    public class Employee{
+        public string  EmployeeId { get; set; }
+        public string FullName { get; set; }
+    }
+}
