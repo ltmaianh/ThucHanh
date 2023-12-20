47e1ca74855c197c68149066353625004d26eaaa
ltmaianh <maianh2k2tx@gmail.com>
2023-12-21 01:14:26 +0700

    phan_trang_pagesize

--- DemoMVC/Controllers/PersonController.cs
+++ DemoMVC/Controllers/PersonController.cs
@@ -24,11 +24,23 @@ namespace DemoMVC.Controllers
         private ExcelProcess _excelPro = new ExcelProcess();
 
         // GET: Person
-        public async Task<IActionResult> Index(int? page){
-            var model = _context.Person.ToList().ToPagedList(page ?? 1,5);
+        //le thi mai anh-2021050076
+       public async Task<IActionResult> Index(int? page, int? PageSize)
+        {
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+                new SelectListItem() { Value="3", Text="3"},
+                new SelectListItem() { Value="5", Text="5"},
+                new SelectListItem() { Value="10", Text="10"},
+                new SelectListItem() { Value="15", Text="15"},
+                new SelectListItem() { Value="25", Text="25"},
+                new SelectListItem() { Value="50", Text="50"},
+            };
+            int pagesize = (PageSize ?? 3);
+            ViewBag.psize = pagesize;
+            var model = _context.Person.ToList().ToPagedList(page ?? 1, pagesize);
             return View(model);
         }
-      
         // GET: Person/Details/5
         public async Task<IActionResult> Details(string id)
         {
